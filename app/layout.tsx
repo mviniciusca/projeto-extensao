03e21896377afc4b57262c0767194ef69ae764d2
Marcos Coelho <mviniciusca@gmail.com>
2025-04-22 20:16:23 -0300

    feat: add favicon images and update metadata for improved branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,39 @@ export const metadata: Metadata = {
   title: "Guia de Projetos de Extensão - ADS",
   description: "Guia para desenvolvimento de projetos de extensão alinhados com os ODS da ONU",
   generator: 'Next.js',
+  icons: {
+    icon: [
+      {
+        url: "/favicon/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png"
+      },
+      {
+        url: "/favicon/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png"
+      }
+    ],
+    apple: {
+      url: "/favicon/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png"
+    },
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: "/favicon/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        rel: "android-chrome-512x512",
+        url: "/favicon/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
